6c7115ffeea04a5e7f27a5cfc8ca903b5f3fad77
Rex-Prime <itsrex.dev@gmail.com>
2026-03-23 19:13:00 +0500

    Initial Commit

---
+++ wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 128
+config.initial_rows = 32
+
+config.enable_tab_bar = false
+
+-- or, changing the font size and color scheme.
+config.font_size = 14
+config.font = wezterm.font ('JetBrainsMonoNL Nerd Font Mono', {weight="Regular", stretch="Normal", style="Normal"})
+config.color_scheme = 'Rosé Pine Moon (base16)'
+
+-- Finally, return the configuration to wezterm:
+return config
